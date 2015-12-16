b9b70b28352d6870b035787de3015bc202767ff3
tylermurdock <tylermurdock21@gmail.com>
2015-12-16 15:31:29 -0700

    fixed server merging error

--- server/keys.js
+++
@@ -1,6 +0,0 @@
-var GoogleKeys = {
-	googleId: '984022701109-amba6rvbl0ki4nlkfreqib8hoa9tck1k.apps.googleusercontent.com',
-	googleSecret: 'uj9xwH25oYdSxvqWffNiJpBy', 
-}
-
-module.exports = GoogleKeys;
